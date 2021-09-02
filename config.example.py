3aa6ab89aaebe3a3a03b25e403fd5e741518a1b9
zxxcq <qwe@zxc.com>
2021-09-03 02:18:12 +0300

    123

--- config.example.py
+++ config.example.py
@@ -2,12 +2,6 @@ import requests
 import re
 import base64
 
-def getXenforoCookie():
-    r = requests.get('https://lolz.guru/process-qv9ypsgmv9.js', headers={'User-Agent':'Mozilla/5.0'})
-    cookieArray = re.search('^var _0x\w+=(.*?);', r.text).group(1)
-    base64DfId = eval(cookieArray)[-1]
-    return base64.b64decode(base64DfId).decode()
-
 cokies = 'cookies'
 
 # cookies = {'G_ENABLED_IDPS': 'google', ' xf_tfa_trust': 'tU6gqaK5aJj98D-BaFaba0RKSUC1a6eM', ' xf_user': '3764769,e162b958a9ee984b93a37a7820b120553a66368b', ' xf_logged_in': '1', ' timezoneOffset': '10800,0', ' xf_disable_market_warning_page': '1', ' _gid': 'GA1.2.836018498.1628774032', ' xf_last_read_article_date': '1627904520', ' _ga': 'GA1.1.1170466874.1625776137', ' xf_market_items_viewed': '18161807,18161996,17934252,18161982', ' df_id': getXenforoCookie(), ' xf_session': 'ccc0520b9e8fff02c44fcc010e58e547', ' xf_market_search_url': '/market/user/3764769/items', ' _ga_J7RS527GFK': 'GS1.1.1629912053.373.1.1629912163.0'}
